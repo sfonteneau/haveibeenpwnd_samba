1e1e0881d9d15c541cc9761b394a78434f97e780
sfonteneau <blog@lesfourmisduweb.org>
2018-12-24 10:48:46 +0100

    decode utf8 raw input

--- check_password_hibpwnd.py
+++ check_password_hibpwnd.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from haveibeenpwnd import check_password
 import sys
 
 def main():
-    if check_password(raw_input())['count'] != 0 :
+    if check_password(raw_input().decode('utf8'))['count'] != 0 :
         print('BAD Password try again')
         sys.exit(1)
     else:
