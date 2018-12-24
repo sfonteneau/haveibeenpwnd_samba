9bf239472aaf0f5e8ec2c6ec47d6b2e1629e8da2
sfonteneau <blog@lesfourmisduweb.org>
2018-12-24 10:49:56 +0100

    getpass

--- check_password_hibpwnd.py
+++ check_password_hibpwnd.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from haveibeenpwnd import check_password
 import sys
+import getpass
 
 def main():
-    if check_password(raw_input().decode('utf8'))['count'] != 0 :
+    if check_password(getpass.getpass().decode('utf8'))['count'] != 0 :
         print('BAD Password try again')
         sys.exit(1)
     else:
